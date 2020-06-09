2e92be0257f6c4b97199b6a877b6fa1cb15d889e
tavoGeek <gcnctz@gmail.com>
2020-06-08 21:42:53 -0500

    deploy

--- src/server/server.js
+++ src/server/server.js
@@ -12,6 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Static Files
-app.use('/', express.static(path.join(__dirname, '../dist')));
+app.use('/', express.static(path.join(__dirname, '../../dist')));
 
 module.exports = app;
